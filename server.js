dd0cfafd331e49f848d5c68a6e3306ef4ecd1f71
Alex Chiang <achiang@canonical.com>
2014-07-01 09:20:07 -0700

    server: refactor to support searching 'all' collections
    In order to support searching all modes of transport given a set of
    GPS coordinates, we need to be able to search all mongo collections.
    Due to the async nature of node, this can be difficult, as the old
    code would return as soon as the results from searching one collection
    came back.
    
    So, refactor using an async pattern.
    
    We now support the /all route.

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 var config = require('./config/config');
 var models = require('./models');
 var restify = require('restify');
+var async = require('async');
+var inflection = require('inflection');
 var server = restify.createServer({name: config.name});
+
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
 
 server
     .use(restify.fullResponse())
@@ -12,7 +18,7 @@ server.listen(config.listen_port, function() {
     console.log('%s listening at %s', server.name, server.url);
 });
 
-server.get(/v1\/(bikes|buses|trains|metros)/, function(req, res, next) {
+server.get(/v1\/(all|bikes|buses|trains|metros)/, function(req, res, next) {
     var lat = parseFloat(req.query.lat);
     var lng = parseFloat(req.query.lng);
     var point = {type: 'Point', coordinates: [lng, lat]};
@@ -22,29 +28,30 @@ server.get(/v1\/(bikes|buses|trains|metros)/, function(req, res, next) {
         next();
     }
 
-    switch (req.params[0]) {
-        case "bikes":
-            var model = models.Bike;
-            break;
-        case "buses":
-            var model = models.Bus;
-            break;
-        case "trains":
-            var model = models.Train;
-            break;
-        case "metros":
-            var model = models.Metro;
-            break;
-    }
+    var ret = []
+    if (req.params[0] === "all")
+        var collections = [ models.Bike, models.Train, models.Metro, models.Bus ];
+    else
+        var collections = [ models[inflection.singularize(req.params[0]).capitalize()] ];
 
-    // http://stackoverflow.com/q/22623998/
-    model.geoNear(point, { maxDistance: 100/6378137, spherical: true }
-        , function (err, data) {
-            if (err)
-                return next(new restify.InvalidArgumentError(err.errmsg));
-                
-            res.send(200, data);
-            next();
-        });
+    async.each(collections
+        ,function eachCollection(collection, eachCallback) {
+            // http://stackoverflow.com/q/22623998/
+            collection.geoNear(point, { maxDistance: 100/6378137, spherical: true }
+            ,function(err, data) {
+                if (err)
+                    return eachCallback(err);
+
+                ret = ret.concat(data);
+                eachCallback(null);
+            });
+        }
+    ,function eachFinally(error) {
+        if (error)
+            return next(new restify.InvalidArgumentError(err.errmsg));
+
+        res.send(200, ret);
+        next();
+    });
 });
 
