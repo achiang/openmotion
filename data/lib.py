6dc6e12eafbeb6976058d619e7d5568a647347d2
Alex Chiang <achiang@canonical.com>
2014-07-05 22:27:00 -0700

    data/lib: look in api directory for mongodb config

--- data/lib.py
+++ data/lib.py
@@ -35,7 +35,7 @@ def get_basepath():
     return os.path.dirname(os.path.realpath(__file__)) + '/'
 
 def get_mongo_config():
-    with open(get_basepath() + '../config/config.js') as f:
+    with open(get_basepath() + '../api/config/config.js') as f:
         for line in f:
             if 'mongo_host' in line:
                 mongo_host = line.split(':')[-1].strip().replace("\"", "")
